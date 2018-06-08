47c3f08701273d142eb3e7e985a8f011fe5cc437
bsongras <ben.songras@gmail.com>
2018-06-07 23:44:52 -0700

    Completed 2-5.c

--- exercises/2-5.c
+++ exercises/2-5.c
@@ -6,14 +6,44 @@ Date: 6/6/18
 
 #include <stdio.h>
 
+#define MAXLINE 1000
+
 int getLine(char s[], int lim);
+int any(char s1[], char s2[]);
 
 void main()
 {
+	int match;
+	char str1[MAXLINE];
+	char str2[MAXLINE];
 
+	getLine(str1, MAXLINE);
+	getLine(str2, MAXLINE);
+	match = any(str1, str2);
+
+	printf("The strings match at: %d\n", match);
 }
 
-void 
+int any(char s1[], char s2[])
+{
+	int i, len;
+	len = 0;
+
+	for(i = 0; s1[i] != '\0'; i++)
+	{
+		if(s1[i] == s2[i])
+		{
+			len = i;
+			break;
+		}
+	}
+
+	if (len == 0 && i > 0)
+	{
+		len = -1;
+	}
+	return len;
+}
 
 int getLine(char s[], int lim)
 {
